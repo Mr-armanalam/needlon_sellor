fb13e548d78312736b07ca8e22b778dbca3bfb6a
Arman Alam <armanalam78578@gmail.com>
2026-06-24 21:51:08 +0530

    feat: add useCurrentUser hook and integrate it into LoginButton component

--- modules/Navbar/components/LoginButton.tsx
+++ modules/Navbar/components/LoginButton.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { User } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const LoginButton = () => {
-  
+  const router = useRouter();
+  const { user } = useCurrentUser();
+
+  if (user?.email) return null;
+
   return (
-    <Button variant={"ghost"} className="text-lg py-3 cursor-pointer">
+    <Button
+      onClick={() => router.push("/login")}
+      variant={"ghost"}
+      className="text-lg py-3 cursor-pointer"
+    >
       <User />
       Login
     </Button>
